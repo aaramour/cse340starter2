ca487c6c72b2639ed7868c0016392cf83c023ade
aaramour@byui.edu <aaramour@byui.edu>
2024-05-07 17:08:02 -0400

    W02 Complete

--- routes/static.js
+++ routes/static.js
@@ -1,14 +1,14 @@
-const express = require('express');
-const router = express.Router();
+const express = require('express'); //The express package is brought into the scope of the file and assigned to a local variable
+const router = express.Router(); //The Express "router" functionality is invoked and stored into a local variable for use
 
 // Static Routes
 // Set up "public" folder / subfolders for static files
-router.use(express.static("public"));
-router.use("/css", express.static(__dirname + "public/css"));
-router.use("/js", express.static(__dirname + "public/js"));
-router.use("/images", express.static(__dirname + "public/images"));
+router.use(express.static("public")); //express is directed to use the 'express.static' function from the public folder
+router.use("/css", express.static(__dirname + "public/css")); //any route that contains /css is to refer to the public/css folder
+router.use("/js", express.static(__dirname + "public/js")); //any route that contains /js is to refer to the public/js folder
+router.use("/images", express.static(__dirname + "public/images")); //any route that contains /images is to refer to the public/images folder
 
-module.exports = router;
-
+module.exports = router; //Exports the router object, along with all of these use statements
+//This is VERY IMPORTANT. If a resource is NOT exported, it cannot be used somewhere else
 
 
--- server.js
+++ server.js
@@ -9,11 +9,23 @@ const express = require("express")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
+const expressLayouts = require("express-ejs-layouts")
+
+/* ***********************
+ * View Engine and Templates
+ *************************/
+app.set("view engine", "ejs") //we declare that ejs will be the view engine for our application
+app.use(expressLayouts) //tells the application to use the express-ejs-layouts package, which has been stored into a "expressLayouts" var
+app.set("layout", "./layouts/layout") // not at views root //express ejs will find the basic template for a view in a layouts folder
 
 /* ***********************
  * Routes
  *************************/
 app.use(static)
+// Index route
+app.get("/", function(req, res){
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
